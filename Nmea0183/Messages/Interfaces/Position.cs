d3bb757d8236dcdde860ec6bf1267892cceed149
windsunrain <windsunrain@gmail.com>
2016-04-10 21:14:37 -0700

    oops include new position class

---
+++ Nmea0183/Messages/Interfaces/Position.cs
@@ -0,0 +1,29 @@
+﻿using Nmea0183.Messages.Enum;
+
+namespace Nmea0183.Messages.Interfaces
+{
+  public class Position
+  {
+    public double Latitude { get; set; }
+    public NorthSouth LatitudeHemisphere { get; set; }
+    public double Longitude { get; set; }
+    public EastWest LongitudeHemisphere { get; set; }
+
+    public Position()
+    {
+    }
+
+    public Position(string lat, string ns, string lon, string ew)
+    {
+      Latitude = double.Parse(lat);
+      LatitudeHemisphere = MessageBase.ParseOneLetterEnumByValue<NorthSouth>(ns);
+      Longitude = double.Parse(lon);
+      LongitudeHemisphere = MessageBase.ParseOneLetterEnumByValue<EastWest>(ew);
+    }
+
+    public override string ToString()
+    {
+      return $"{Latitude:F3},{MessageBase.F(LatitudeHemisphere)},{Longitude: F3},{MessageBase.F(LongitudeHemisphere)}";
+    }
+  }
+}
