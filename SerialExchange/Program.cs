b35ad5f58382ee808fd908347f088bba1329fdfa
MichielAtMove <windsunrain@gmail.com>
2016-08-13 12:21:18 -0700

    vast improvements to serialexchange.

--- SerialExchange/Program.cs
+++ SerialExchange/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Ports;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Nmea0183.Helpers;
 using RabbitMQ.Client;
@@ -14,9 +15,9 @@ using RabbitMQ.Client.Framing;
 
 namespace SerialExchange
 {
-  class Program
+  internal class Program
   {
-    static int Main(string[] args)
+    private static int Main(string[] args)
     {
       var commandName = Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
 
@@ -40,74 +41,127 @@ namespace SerialExchange
       var nameReceiveExchange = args[1];
       var nameSendExchange = args[2];
 
-      using (var port = new SerialPort(namecomport, 115200))
+      var stopevent = new ManualResetEventSlim();
+      var task = RunSerialExchanger(namecomport, 115200, nameReceiveExchange, nameSendExchange, stopevent, reopenporttimeout: 10, onException: exception =>
+        {
+          var message = exception.ToString();
+          Trace.WriteLine(message);
+          Console.Error.WriteLine(message);
+        }
+      );
+      Console.WriteLine(
+        "{0} active.\nSerial port '{1}' -> exchange '{2}'.\nExchange '{3}' -> serial port '{1}'\nPress Q to exit.",
+        commandName, namecomport, nameReceiveExchange, nameSendExchange);
+
+      while (Console.ReadKey().KeyChar.ToString().ToLower() != "q")
+        ;
+      stopevent.Set();
+      task.Wait();
+      return 0;
+    }
+
+    private static Task RunSerialExchanger(string namecomport, int baudrate, string nameReceiveExchange,
+      string nameSendExchange, ManualResetEventSlim stopevent, Action<Exception> onException = null, int reopenporttimeout = 0)
+    {
+      var task = Task.Factory.StartNew(() =>
       {
         try
         {
+          var factory = new ConnectionFactory { HostName = "localhost" };
+          using (var connection = factory.CreateConnection())
+          using (var model = connection.CreateModel())
+          {
+            model.ExchangeDeclare(nameReceiveExchange, "fanout");
+            model.ExchangeDeclare(nameSendExchange, "fanout");
 
-          port.Open();
+            // port will be recreated each time we time out
+            using (var timeout = new TimeOut(reopenporttimeout))
+            {
+              Func<SerialPort> createport = () =>
+              {
+                var p = new SerialPort(namecomport, baudrate);
+                p.Open();
+                return p;
+              };
+
+              var port = createport();
+              timeout.Elapsed += () =>
+              { port.Close();
+                port.Dispose();
+                port = createport();
+              };
+
+              #region Write to serial port
+
+              var queueName = model.QueueDeclare().QueueName;
+              model.QueueBind(queueName, nameReceiveExchange, string.Empty);
+
+              var consumer = new EventingBasicConsumer(model);
+              consumer.Received += (m, ea) =>
+              {
+                var message = Encoding.UTF8.GetString(ea.Body);
+                port.WriteLine(message);
+              };
+
+              model.BasicConsume(queueName, true, consumer);
+
+              #endregion
+
+              #region Receive from serial port
+
+              var linebuffer = new StringBuilder(1024);
+
+              port.ErrorReceived += (sender, args) =>
+              {
+                var error = $"Error received: {args.ToString()}";
+                Console.Error.WriteLine(error);
+                Trace.WriteLine(error);
+              };
+
+              port.DataReceived += (sender, eventArgs) =>
+              {
+                timeout?.Reset();
+
+                var data = port.ReadExisting();
+
+                var lines = data.Split('\n').ToList();
+
+                while (lines.Any())
+                {
+                  linebuffer.Append(lines[0]);
+                  lines.RemoveAt(0);
+                  if (!lines.Any())
+                    continue;
+                  var line = linebuffer.ToString().Trim();
+
+                  var body = Encoding.UTF8.GetBytes(line);
+
+                  var basicProperties = new BasicProperties {Timestamp = DateTime.Now.AmqpTimestamp()};
+                  model.BasicPublish(nameReceiveExchange, string.Empty, basicProperties, body);
+
+                  linebuffer.Clear();
+                }
+              };
+
+              #endregion
+
+              stopevent.Wait();
+              port.Close();
+              port.Dispose();
+            }
+          }
         }
         catch (Exception x)
-        { Trace.WriteLine(x.ToString());
-          Console.Error.WriteLine(x.ToString());
-          return -1;
-        }
-
-        var factory = new ConnectionFactory() {HostName = "localhost"};
-        using (var connection = factory.CreateConnection())
-        using (var model = connection.CreateModel())
         {
-          model.ExchangeDeclare(nameReceiveExchange, "fanout");
-          model.ExchangeDeclare(nameSendExchange, "fanout");
-
-          {
-            var queueName = model.QueueDeclare().QueueName;
-            model.QueueBind(queue: queueName,
-              exchange: nameReceiveExchange,
-              routingKey: string.Empty);
-
-            var consumer = new EventingBasicConsumer(model);
-            consumer.Received += (m, ea) =>
-            {
-              var message = Encoding.UTF8.GetString(ea.Body);
-              port.WriteLine(message);
-            };
-
-            model.BasicConsume(queue: queueName, noAck: true, consumer: consumer);
-          }
-
-          var stop = false;
-
-          using (var task = Task.Factory.StartNew(() =>
-          {
-            // ReSharper disable once AccessToModifiedClosure
-            while (!stop)
-            {
-              var line = port.ReadLine();
-
-              var body = Encoding.UTF8.GetBytes(line);
-              // ReSharper disable once AccessToDisposedClosure
-              var basicProperties = new BasicProperties() {Timestamp = DateTime.Now.AmqpTimestamp()};
-              model.BasicPublish(exchange: nameReceiveExchange,
-                routingKey: String.Empty,
-                basicProperties: basicProperties,
-                body: body);
-
-            }
-            // ReSharper disable once FunctionNeverReturns
-          }))
-          {
-
-            Console.WriteLine(
-              "{0} active.\nSerial port '{1}' -> exchange '{2}'.\nExchange '{3}' -> serial port '{1}'\nPress any key to exit.",
-              commandName, namecomport, nameReceiveExchange, nameSendExchange);
-            Console.ReadKey();
-            stop = true;
-          }
+          if (onException != null)
+            onException(x);
+          else
+            throw;
         }
+      });
 
-        return 0;
-      }
+
+      return task;
     }
   }
 }
---
+++ SerialExchange/TimeOut.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace SerialExchange
+{
+  internal class TimeOut : IDisposable
+  {
+    private readonly Timer _timer;
+
+    public TimeOut(int seconds)
+    {
+      _timer = new Timer(1000 * seconds) { AutoReset = false };
+      _timer.Elapsed += (sender, args) =>
+      {
+        Elapsed?.Invoke();
+        Reset();
+      };
+      _timer.Start();
+    }
+
+    public delegate void ElapsedEventHandler();
+
+    public event ElapsedEventHandler Elapsed; 
+
+    public void Reset()
+    {
+      _timer.Stop();
+      _timer.Start();
+    }
+
+    public void Dispose()
+    {
+      _timer.Stop();
+      _timer.Dispose();
+    }
+  }
+}
